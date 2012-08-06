0678cf980f5c163d4dea02b986f5a04c8e181236
Victor Stabile <vmstabile@gmail.com>
2012-08-06 19:41:14 -0300

    dados do portador adicionados a chamada de transacao

--- lib/cielo/transaction.rb
+++ lib/cielo/transaction.rb
@@ -7,8 +7,15 @@ module Cielo
     def create!(parameters={})
       analysis_parameters(parameters)
       message = xml_builder("requisicao-transacao") do |xml|
+        xml.tag!("dados-portador") do
+          [:"nome-portador", :numero, :validade, :"codigo-seguranca"].each do |key|
+            xml.tag!(key.to_s, parameters[key].to_s)
+          end
+          xml.indicador 1
+        end
         xml.tag!("dados-pedido") do
-          [:numero, :valor, :moeda, :"data-hora", :idioma].each do |key|
+          xml.numero parameters[:pedido].to_s
+          [:valor, :moeda, :"data-hora", :descricao, :idioma].each do |key|
             xml.tag!(key.to_s, parameters[key].to_s)
           end
         end
@@ -43,24 +50,26 @@ module Cielo
     
     private
     def analysis_parameters(parameters={})
-      [:numero, :valor, :bandeira, :"url-retorno"].each do |parameter|
+      [:"nome-portador", :numero, :validade, :"codigo-seguranca", :pedido, :valor, :bandeira].each do |parameter|
         raise Cielo::MissingArgumentError, "Required parameter #{parameter} not found" unless parameters[parameter]
       end
       parameters.merge!(:moeda => "986") unless parameters[:moeda]
       parameters.merge!(:"data-hora" => Time.now.strftime("%Y-%m-%dT%H:%M:%S")) unless parameters[:"data-hora"]
+      parameters.merge!(:descricao => "") unless parameters[:descricao]
       parameters.merge!(:idioma => "PT") unless parameters[:idioma]
       parameters.merge!(:produto => "1") unless parameters[:produto]
       parameters.merge!(:parcelas => "1") unless parameters[:parcelas]
       parameters.merge!(:autorizar => "2") unless parameters[:autorizar]
       parameters.merge!(:capturar => "true") unless parameters[:capturar]
       parameters.merge!(:"url-retorno" => Cielo.return_path) unless parameters[:"url-retorno"]
+      parameters[:bandeira].downcase!
       parameters
     end
     
     def xml_builder(group_name, target=:after, &block)
       xml = Builder::XmlMarkup.new
       xml.instruct! :xml, :version=>"1.0", :encoding=>"ISO-8859-1"
-      xml.tag!(group_name, :id => "#{Time.now.to_i}", :versao => "1.1.0") do
+      xml.tag!(group_name, :id => "#{Time.now.to_i}", :versao => "1.1.1") do
         block.call(xml) if target == :before
         xml.tag!("dados-ec") do
           xml.numero Cielo.numero_afiliacao
